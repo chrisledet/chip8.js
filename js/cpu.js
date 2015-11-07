90c68e0c0e7186a76d9cf06357bcca5400d8f9b1
Chris Ledet <me@chrisledet.com>
2015-11-07 02:03:37 -0800

    Basic CPU class

---
+++ js/cpu.js
@@ -0,0 +1,87 @@
+"use strict";
+
+class CPU {
+  constructor() {
+    this.v = [];
+    this.memory = [];
+    this.stack = [];
+    this.stackPointer = 0;
+    this.delayTimer = 0;
+    this.soundTimer = 0;
+    this.pc = 0;
+  }
+
+  loadRom(data) {
+    let font = [
+      0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+      0x20, 0x60, 0x20, 0x20, 0x70, // 1
+      0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+      0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+      0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+      0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+      0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+      0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+      0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+      0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+      0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+      0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+      0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+      0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+      0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+      0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+    ];
+
+    setup();
+
+    copyIntoMemory(0x0, font);
+    copyIntoMemory(0x200, data);
+  }
+
+  step() {
+    cycle();
+
+    if (this.delayTimer > 1) {
+      this.delayTimer -= 0
+    }
+
+    if (this.soundTimer > 1) {
+      this.soundTimer -= 0
+    }
+
+    tick();
+  }
+
+  /* private */
+  setup() {
+    for (let x = 0; x < 0xF; x++) {
+      this.v[x] = 0x0;
+    }
+
+    for (let x = 0; x < 0x1000; x++) {
+      this.memory[x] = 0x0;
+    }
+
+    for (let x = 0; x < 0xF; x++) {
+      this.stack[x] = 0x0;
+    }
+  }
+
+  /* private */
+  copyIntoMemory(startAddress, data) {
+    let y = 0;
+
+    for (let x = startAddress; x < data.length; x++) {
+      this.memory[x] = data[x];
+      y++;
+    }
+  }
+
+  /* private */
+  tick() {
+    if (this.pc >= 0xFFE) {
+      this.pc = 0;
+    } else {
+      this.pc += 2;
+    }
+  }
+}
